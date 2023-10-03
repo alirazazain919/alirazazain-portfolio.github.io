0ce8f6213a4547574814fdbaf48afd31e0d8d0ea
Ali Raza Zain <alirazazain919@gmail.com>
2023-10-03 12:03:47 +0500

    My Portfolio Website

---
+++ main.js
@@ -0,0 +1,9 @@
+﻿var typed= new Typed(".text", {
+	strings:[ "Frontend Developer" , "Php Developer" , "Web Developer" ,"Youtuber"],
+	typeSpeed:100,
+	backSpeed:100,
+	backDelay:1000,
+	loop:true
+}
+	
+);
